47603cc25a21dc32553086d11d44fe1716374119
galarcelucas@gmail.com <galarcelucas@gmail.com>
2020-04-25 23:53:15 -0300

    identado

--- validar.js
+++ validar.js
@@ -4,12 +4,12 @@ let flag;
 
 document.getElementById('myBtn').addEventListener("click", () => {
     let textoAValidar = text.value;
-    
-    if(textoAValidar.indexOf(",",) === -1 || textoAValidar.split(',').length != 2 ) { //Valido que haya solo una coma.
+
+    if (textoAValidar.indexOf(",") === -1 || textoAValidar.split(',').length != 2) { //Valido que haya solo una coma.
         console.log('Falta la coma');
         text.style.border = "2px solid red";
         flag = false;
-    } else if(textoAValidar.indexOf(",",) < 2 || (textoAValidar[textoAValidar.indexOf(",",)+3]) == undefined) { //Valido que haya aunque sea 2 caracteres detras y adelante de la coma
+    } else if (textoAValidar.indexOf(",") < 2 || (textoAValidar[textoAValidar.indexOf(",") + 3]) == undefined) { //Valido que haya aunque sea 2 caracteres detras y adelante de la coma
         console.log("Error de validacion");
         text.style.border = "2px solid red";
         flag = false;
@@ -20,9 +20,9 @@ document.getElementById('myBtn').addEventListener("click", () => {
     }
     // hago un eventlistener para el boton Formatear, que depende del flag
     document.getElementById('myFormat').addEventListener('click', () => {
-        if(flag)
-        parrafo.innerHTML = textoAValidar.split(',')[1] + ' ' + textoAValidar.split(',')[0];
+        if (flag)
+            parrafo.innerHTML = textoAValidar.split(',')[1] + ' ' + textoAValidar.split(',')[0];
         else
-        parrafo.innerHTML = 'Dato no valido'
+            parrafo.innerHTML = 'Dato no valido'
     });
 });
